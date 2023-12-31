fd43437161d2d9f8bafd93339071f5e6e3ed53e2
Pigg <thestream1005@gmail.com>
2023-12-31 20:22:05 +0700

    a

--- Server.js
+++ Server.js
@@ -2,14 +2,15 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({
   port: 8080
 });
-
+console.log("running")
 let sockets = [];
 server.on('connection', function(socket) {
   sockets.push(socket);
-
+  console.log(socket.listenerCount)
   
   socket.on('message', function(msg) {
     sockets.forEach(s => s.send(msg));
+    console.log(msg)
   });
 
   
